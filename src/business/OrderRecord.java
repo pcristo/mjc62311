fb46abc715a31e571f0e575d6836acfa829485ed
pcristo <patrickc@encs.concordia.ca>
2015-05-27 22:40:58 -0400

    Fixed XML serialisation issue in Business class

--- src/business/OrderRecord.java
+++ src/business/OrderRecord.java
@@ -3,6 +3,7 @@ package business;
 import java.io.Serializable;
 
 import share.ShareOrder;
+import share.ShareType;
 
 /**
  * A class to store a record of an order, including whether or not it is paid
@@ -12,6 +13,13 @@ import share.ShareOrder;
 public class OrderRecord extends ShareOrder implements Serializable {
 	private static final long serialVersionUID = 1L;		// required for serialization
 	private boolean isPaid;
+	
+	/**
+	 * Default constructor needed for serialization to XML. Do not call. 
+	 */
+	public OrderRecord() {
+		super(null, null, null, ShareType.PREFERRED, 0, 0, 0);
+	}
 	
 	public OrderRecord(ShareOrder sO, boolean isPaid) {
 		super(sO.getOrderNum(), sO.getBrokerRef(), sO.getBusinessSymbol(), sO.getShareType(),sO.getUnitPrice(),
