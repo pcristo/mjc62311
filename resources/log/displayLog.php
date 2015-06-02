10566219c1c925a8475e3ce5a44b28d2854f4fdb
Ross Smith <rbrsmith@gmail.com>
2015-06-02 14:19:39 -0400

    Added ui for clearing log file

--- resources/log/displayLog.php
+++ resources/log/displayLog.php
@@ -1,9 +1,14 @@
 <?php
 $string = file_get_contents("C:\\Users\\b0467851\\WORK\\school\\DistributedSystems\\project\\src\\config.json");
 
+$wipe = $_GET['wipe'];
+
 $json = json_decode($string, true);
 $logFileLocation = $json['logServerHome'] . $json['logServerFile'];
 
+if($wipe == "true") {
+	file_put_contents($logFileLocation, "");
+}
 
 //$log = file_get_contents($logFileLocation);
 
