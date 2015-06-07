e06fecd755b8e249b5f1d4c81f1d93982d199118
pcristo <patrick@cristofaro.net>
2015-06-07 18:54:23 -0400

    Update projectLauncher.java

--- src/projectLauncher.java
+++ src/projectLauncher.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.rmi.RemoteException;
 
 import client.BrokerServiceClient;
@@ -5,13 +6,17 @@ import stockexchange.broker.Broker;
 import business.Business;
 import logger.LoggerServer;
 
+/**
+ * Run this class either in debug or regular mode to set up all your servers. Works on
+ * all platforms.
+ * @author patrick
+ */
 public class projectLauncher {
 	final static int PAUSE_NOTIFICATION_TIME = 250;
 	final static int WAIT_BETWEEN_LAUNCH_TIME = 2000;
 	final static String PREFACE_STRING = "******\t";
 	
-	public static void main(String[] args) throws InterruptedException,
-			RemoteException {
+	public static void main(String[] args) throws InterruptedException,	IOException {
 		
 		// Launch threads
 		Thread logger = new Thread(new Runnable() {
@@ -45,13 +50,23 @@ public class projectLauncher {
 			// let everything get rolling before starting the client
 			System.out.println(PREFACE_STRING + "Client starting... ");
 
-			BrokerServiceClient.main(null);
+			//BrokerServiceClient.main(null);
 		}
+		
+		System.out.println("Press enter to kill everything uncleanly :D");
+		System.in.read();
+		
+		broker.stop();
+		business.stop();
+		logger.stop();
+			
+		System.out.println("Okay everyone is dead.");
+		System.exit(1);
 	}
 
 	private static void pause(String msg, int ms) throws InterruptedException {		
 		for (int t = ms; t > 0; t -= PAUSE_NOTIFICATION_TIME) {
-			System.out.println(PREFACE_STRING + msg + t + " ms");
+			// System.out.println(PREFACE_STRING + msg + t + " ms");
 			Thread.sleep(PAUSE_NOTIFICATION_TIME);
 		}
 	}
