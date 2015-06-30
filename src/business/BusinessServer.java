73275279851d55fc885e35b96510b2e3d93bf87a
pcristo <patrickc@encs.concordia.ca>
2015-06-29 21:27:41 -0400

    register to exchange (partial implementation)

--- src/business/BusinessServer.java
+++ src/business/BusinessServer.java
@@ -2,71 +2,21 @@ package business;
 
 import org.omg.CORBA.*;
 import org.omg.CORBA.Object;
+import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 /**
- * This class creates a new instance of a business, creates the ORB, 
- * registers with the CORBA Naming Service, and runs the server in 
- * a background thread
- *  
+ * This class creates a new instance of a business, creates the ORB, registers
+ * with the CORBA Naming Service, and runs the server in a background thread
+ * 
  * @see http://www.javacoffeebreak.com/articles/javaidl/javaidl.html
  * @author patrick
  */
 public class BusinessServer implements Runnable {
 	private BusinessServant business;
-	private String businessSymbol = "";
-	
-	/**
-	 * Initializes the ORB, connects to naming service, keeps 
-	 * the server alive. You should call the static method launch(String symbol)
-	 * instead of calling run directly
-	 */
-	@Override
-	public void run() {
-		try {
-			// Create ORB
-			ORB orb = ORB.init(new String[0], null);
 
-			// Connect object to ORB
-			orb.connect((Object) business);
-
-			// TODO: patrickc log the event
-
-			// Get nameservice reference
-			org.omg.CORBA.Object object = orb
-					.resolve_initial_references("NameService");
-
-			// Cast to naming context
-			NamingContext namingContext = NamingContextHelper.narrow(object);
-
-			// Add a new naming component for our interface
-			NameComponent list[] = { new NameComponent("business-" + businessSymbol, "") };
-
-			// Now notify naming service of our new interface
-			namingContext.rebind(list, (Object) business);
-
-		} catch (Exception e) {
-			System.err.println("ORB Error - " + e);
-			e.printStackTrace(System.out);
-			System.exit(0);
-		}
-		
-		// loop forever
-		while (true) {}
-	}
-	
-	/**
-	 * Sets the business symbol handled by this server. Can only be set once.
-	 * 
-	 * @param businessSymbol
-	 */
-	protected void setBusinessSymbol(String businessSymbol) {
-		if (this.businessSymbol.equals("")) {
-			this.businessSymbol = businessSymbol;
-		}
-	}
-		
 	/**
 	 * Launches a new business server within a new thread.
 	 * 
@@ -83,4 +33,92 @@ public class BusinessServer implements Runnable {
 
 		return thread;
 	}
+
+	/**
+	 * Initializes the ORB, connects to naming service, keeps the server alive.
+	 * Registers the business with the exchange. You should call the static
+	 * method launch(String symbol) instead of calling run directly
+	 */
+	@Override
+	public void run() {
+		try {
+			InitORB();
+			RegisterExchange();
+		} catch (Exception e) {
+			// TODO patrickc log this in the logger
+			System.err.println("Business Server Error - " + e);
+			e.printStackTrace(System.out);
+			System.exit(0);
+		}
+
+		// loop forever
+		while (true) {
+		}
+	}
+
+	/**
+	 * Sets the business symbol handled by this server. Can only be set once.
+	 * 
+	 * @param businessSymbol
+	 */
+	protected void setBusinessSymbol(String businessSymbol) {
+		if (business.getTicker().equals(""))
+			business.setBusinessSymbol(businessSymbol);
+	}
+
+	/**
+	 * Initializes ORB and registers to naming service
+	 * 
+	 * @throws InvalidName
+	 * @throws NotFound
+	 * @throws CannotProceed
+	 * @throws org.omg.CosNaming.NamingContextPackage.InvalidName
+	 */
+	private void InitORB() throws InvalidName, NotFound, CannotProceed,
+			org.omg.CosNaming.NamingContextPackage.InvalidName {
+		// Create ORB
+		ORB orb = ORB.init(new String[0], null);
+
+		// Connect object to ORB
+		orb.connect((Object) business);
+
+		// TODO: patrickc log the event
+
+		// Get nameservice reference
+		org.omg.CORBA.Object object = orb
+				.resolve_initial_references("NameService");
+
+		// Cast to naming context
+		NamingContext namingContext = NamingContextHelper.narrow(object);
+
+		// Add a new naming component for our interface
+		NameComponent list[] = { new NameComponent("business-"
+				+ business.getTicker(), "") };
+
+		// Now notify naming service of our new interface
+		namingContext.rebind(list, (Object) business);
+	}
+
+	/**
+	 * 
+	 */
+	private void RegisterExchange() {
+		// Get the exchange object
+		ORB orb = ORB.init(new String[0], null);
+
+		org.omg.CORBA.Object object = orb
+				.resolve_initial_references("NameService");
+
+		NamingContext namingContext = NamingContextHelper.narrow(object);
+
+		NameComponent nc_array[] = { new NameComponent("exchange", "") };
+
+		org.omg.CORBA.Object objectReference = namingContext.resolve(nc_array);
+
+		iExchange exchange = iExchangeHelper.narrow(objectReference);
+
+		// Register to it
+
+	}
+
 }
