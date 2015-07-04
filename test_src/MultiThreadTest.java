c1c23c3e2c5265988e8dcad3c6a7f7f555b6028b
ghazan <gay.hazan@gmail.com>
2015-07-04 08:49:29 -0400

    Changes Setup Method to Static
    Also made iBroker static

--- test_src/MultiThreadTest.java
+++ test_src/MultiThreadTest.java
@@ -26,10 +26,10 @@ public class MultiThreadTest {
 	final static int NUMBER_OF_TEST_THREADS = 15;
 	final static int SLEEP_TIME_BETWEEN_TRIES = 0;
 	final static int NUMBER_OF_TRANSACTIONS_PER_THREAD = 90000;
-	iBroker broker;
+	static iBroker broker;
 	
 	@BeforeClass
-	public void setUp() throws Exception {
+	public static void setUp() throws Exception {
 		// start up everything
 		projectLauncher.main(new String[1]);
 		
